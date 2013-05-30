7b2781e88ae0bd2103b1c0d3965e2057dee76769
Matt Hicks <mhicks@redhat.com>
2013-05-30 16:06:32 -0400

    Visual tweaks
    - A little more gravity and a larger view
    - More test data to show rendering massive graph

--- src/main/webapp/js/scale.js
+++ src/main/webapp/js/scale.js
@@ -4,48 +4,48 @@
  * @returns {Boolean} whether or not a redraw is needed
  */
 function merge(data) {
-	// Whether or not to redraw the layout
-	var redraw = false;
-	
-	// First, flatten the data structure
-	var nodes = flatten(data);
-	
-	// Merge new data, ignoring gear and application updates
-	// since the updates will overwrite their x & y coordinate
-	// and cause the graph to destabilize
-	for (var i=0; i < nodes.length; i++) {
-		var node = nodes[i];
-		
-		if (node.type == "hit") {
-			root.push(node);
-			redraw = true;
-		} else if (node.type == "gear") {
-			if (!(node.uuid in gears)) {
-				root.push(node);
-				// Cache the gear
-	    		gears[node.uuid] = node;
-	    		if (application) {
-	    			// This might be a new gear that got created
-	    			if (application.children.indexOf(node) < 0) {
-	    				// The application doesn't have this gear, add it
-	    				application.children.push(node);
-	    			}
-	    		}
-	    		redraw = true;
-			} else {
-				// Merge the hits
-				var cacheGear = gears[node.uuid];
-				cacheGear.children = cacheGear.children.concat(node.children);
-			}
-		} else if (node.type == "application") {
-			if (!application) {
-				root.push(node);
-				application = node;
-				redraw = true;
-			}
-		} 
-	}
-	return redraw;
+  // Whether or not to redraw the layout
+  var redraw = false;
+  
+  // First, flatten the data structure
+  var nodes = flatten(data);
+  
+  // Merge new data, ignoring gear and application updates
+  // since the updates will overwrite their x & y coordinate
+  // and cause the graph to destabilize
+  for (var i=0; i < nodes.length; i++) {
+    var node = nodes[i];
+    
+    if (node.type == "hit") {
+      root.push(node);
+      redraw = true;
+    } else if (node.type == "gear") {
+      if (!(node.uuid in gears)) {
+        root.push(node);
+        // Cache the gear
+          gears[node.uuid] = node;
+          if (application) {
+            // This might be a new gear that got created
+            if (application.children.indexOf(node) < 0) {
+              // The application doesn't have this gear, add it
+              application.children.push(node);
+            }
+          }
+          redraw = true;
+      } else {
+        // Merge the hits
+        var cacheGear = gears[node.uuid];
+        cacheGear.children = cacheGear.children.concat(node.children);
+      }
+    } else if (node.type == "application") {
+      if (!application) {
+        root.push(node);
+        application = node;
+        redraw = true;
+      }
+    } 
+  }
+  return redraw;
 }
 
 /**
@@ -58,16 +58,16 @@ function merge(data) {
  */
 function flatten(data) {
     var nodes = [];
-	
+  
     function recurse(node) {
-    	if (node.type == 'gear' || node.type == 'application') {
-    		node.size = node.children.reduce(function(p, v) {return p + recurse(v); }, 0);
-    	} else if (node.type == 'hit') {
-    		node.size = node.count;
-    	}
-		nodes.push(node);
-		return node.size;
-	}
+      if (node.type == 'gear' || node.type == 'application') {
+        node.size = node.children.reduce(function(p, v) {return p + recurse(v); }, 0);
+      } else if (node.type == 'hit') {
+        node.size = node.count;
+      }
+    nodes.push(node);
+    return node.size;
+  }
 
     data.size = recurse(data);
 
@@ -82,38 +82,38 @@ function update() {
 
     // Update the links
     link = vis.selectAll("line.link")
-    	.data(links, link_key);
+      .data(links, link_key);
 
     // Enter any new links before the gears
     // This keeps them under the gears
     link.enter().insert("svg:line", ".node")
-    	.attr("class", "link")
-    	.attr("x1", function(d) { return d.source.x; })
-    	.attr("y1", function(d) { return d.source.y; })
-    	.attr("x2", function(d) { return d.target.x; })
-    	.attr("y2", function(d) { return d.target.y; });
+      .attr("class", "link")
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
 
     // Exit any old links
     link.exit().remove();
 
     // Update the nodes
     node = vis.selectAll("circle")
-    	.data(root, key)
-    	.style("fill", color);
+      .data(root, key)
+      .style("fill", color);
 
     // Enter any new nodes
     node.enter().append("svg:circle")
-    	.attr("class", style)
-    	.attr("cx", function(d) { return d.x; })
-    	.attr("cy", function(d) { return d.y; })
-    	.attr("r", radius)
-    	.style("fill", color)
-    	.on("click", click)
-    	.call(force.drag);
+      .attr("class", style)
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; })
+      .attr("r", radius)
+      .style("fill", color)
+      .on("click", click)
+      .call(force.drag);
 
     // Update the radius to handle a collapse
     node.transition()
-    	.attr("r", radius);
+      .attr("r", radius);
 
     // Exit any old nodes
     node.exit().remove();
@@ -124,12 +124,12 @@ function update() {
 
 function tick() {
     link.attr("x1", function(d) { return d.source.x; })
-    	.attr("y1", function(d) { return d.source.y; })
-    	.attr("x2", function(d) { return d.target.x; })
-    	.attr("y2", function(d) { return d.target.y; });
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
 
     node.attr("cx", function(d) { return d.x; })
-    	.attr("cy", function(d) { return d.y; });
+      .attr("cy", function(d) { return d.y; });
 }
 
 /**
@@ -139,7 +139,7 @@ function tick() {
  * @returns the unique identifier
  */
 function link_key(d) {
-	return "link_" + key(d.target);
+  return "link_" + key(d.target);
 }
 
 /**
@@ -148,12 +148,12 @@ function link_key(d) {
  * @returns the unique identifier
  */
 function key(d) {
-	if (d.type == 'application') {
-    	return d.name;
+  if (d.type == 'application') {
+      return d.name;
     } else if (d.type == 'gear') {
-    	return d.uuid;
+      return d.uuid;
     } else if (d.type == 'hit'){
-    	return d.id;
+      return d.id;
     }
 }
 
@@ -163,15 +163,15 @@ function key(d) {
  * @returns the color
  */
 function color(d) {
-	if (d.type == 'hit') {
-		return "#fd8d3c";
-	} else if (d._children) {
-		return "#3182bd";
-	} else if (d.type == 'application') {
-		return "#acacac";
-	} else if (d.type == 'gear') {
-		return "#c6dbef";
-	}
+  if (d.type == 'hit') {
+    return "#fd8d3c";
+  } else if (d._children) {
+    return "#3182bd";
+  } else if (d.type == 'application') {
+    return "#acacac";
+  } else if (d.type == 'gear') {
+    return "#c6dbef";
+  }
 }
 
 /**
@@ -180,11 +180,11 @@ function color(d) {
  * @returns the link distance
  */
 function linkDistance(d) {
-	if (d.target.type == 'gear') {
-		return 200;
-	} else if (d.target.type == 'hit') {
-		return 20;
-	}
+  if (d.target.type == 'gear') {
+    return 200;
+  } else if (d.target.type == 'hit') {
+    return 20;
+  }
 }
 
 /**
@@ -196,11 +196,11 @@ function linkDistance(d) {
  * @returns the link strength
  */
 function linkStrength(d) {
-	if (d.target.type == 'gear') {
-		return 0.3;
-	} else if (d.target.type == 'hit') {
-		return 1.0;
-	}
+  if (d.target.type == 'gear') {
+    return 0.3;
+  } else if (d.target.type == 'hit') {
+    return 1.0;
+  }
 }
 
 /**
@@ -213,15 +213,15 @@ function linkStrength(d) {
  * @returns the radius
  */
 function radius(d) {
-	if (d._children) {
-		// If collapsed, show total size
-		return Math.log(d.size) * 10;
-	} else if (d.type == 'application') {
-    	return 25;
+  if (d._children) {
+    // If collapsed, show total size
+    return Math.log(d.size) * 10;
+  } else if (d.type == 'application') {
+      return 25;
     } else if (d.type == 'gear') {
-    	return 15;
+      return 15;
     } else if (d.type == 'hit'){
-    	var result = Math.log(d.size);
+      var result = Math.log(d.size);
       if (result == 0) result = 1;
       return result;
     }
@@ -238,11 +238,11 @@ function radius(d) {
  */
 function charge(d) {
     if (d.type == 'application') {
-    	return 0;
+      return 0;
     } else if (d.type == 'gear') {
-    	return -500;
+      return -500;
     } else if (d.type == 'hit') {
-    	return -75;
+      return -75;
     }
 }
 
@@ -253,10 +253,10 @@ function charge(d) {
  * @returns the css classes as a string
  */
 function style(d) {
-	if (d.type == 'application' || d.type == 'gear') {
-    	return d.type + " node";
+  if (d.type == 'application' || d.type == 'gear') {
+      return d.type + " node";
     } else {
-    	return d.type;
+      return d.type;
     }
 }
 
@@ -271,23 +271,40 @@ function style(d) {
 * @param d the object
 */
 function click(d) {
+    function recurse_remove(node) {
+      if (node.children) {
+        node._children = node.children;
+        node.children = null;
+
+        // Remove children from root as well
+        // and recurse on them
+        for (var i = 0; i < node._children.length; i++) {
+          var child = node._children[i];
+          root.splice(root.indexOf(child), 1);
+          recurse_remove(child);
+        }
+      }
+    }
+
+    function recurse_add(node) {
+      if (node._children) {
+        node.children = node._children;
+        node._children = null;
+
+        // Add children back to root as well
+        // and recurse on them
+        for (var i = 0; i < node.children.length; i++) {
+          var child = node.children[i];
+          root.push(child);
+          recurse_add(child);
+        }
+      }
+    }
+
     if (d.children) {
-    	d._children = d.children;
-    	d.children = null;
-
-    	// Remove children from root as well
-    	for (var i = 0; i < d._children.length; i++) {
-    		var child = d._children[i];
-    		root.splice(root.indexOf(child), 1);
-    	}
+      recurse_remove(d);
     } else {
-    	d.children = d._children;
-    	d._children = null;
-
-    	// Add children back to root as well
-    	for (var i = 0; i < d.children.length; i++) {
-    		root.push(d.children[i]);
-    	}
+      recurse_add(d);
     }
     update();
 }
@@ -298,35 +315,35 @@ function click(d) {
 */
 function poll() {
     setTimeout(function() {
-    	// Store the time before calling
-    	newTime = Date.now();
+      // Store the time before calling
+      newTime = Date.now();
 
-		$.ajax({
-		    url : "rest/display/" + time,
-		    success : function(data) {
-		    	// Only redraw if necessary
-		    	if (merge(data)) update();
-				
+    $.ajax({
+        url : "rest/display/" + time,
+        success : function(data) {
+          // Only redraw if necessary
+          if (merge(data)) update();
+
           // Set the new time to get delta data
           time = newTime;
 
           // Setup next poll
           poll();
-		    },
-		    error : function(error) {
+        },
+        error : function(error) {
           // Setup another poll
           poll();
-		    },
-		    dataType : "json"
-		});
+        },
+        dataType : "json"
+    });
     }, 1000);
 }
 
 function pollLocal(selection) {
-		$.getJSON('data' + selection + '.json', function(data) {
-		    	// Only redraw if necessary
-		    	if (merge(data)) update();
-    });	
+    $.getJSON('data' + selection + '.json', function(data) {
+          // Only redraw if necessary
+          if (merge(data)) update();
+    }); 
 }
 
 /**
@@ -334,27 +351,27 @@ function pollLocal(selection) {
 *------------------------- MAIN PROGRAM ------------------------------
 *---------------------------------------------------------------------
 */
-var w = 960, h = 550, link, root = [], application, gears = {};
+var w = 960, h = 600, link, root = [], application, gears = {};
 
 // The time from which to start retrieving results
 var time = 0;
 
 // Build a force layout
 var force = d3.layout.force()
-			.on("tick", tick)
-			.linkDistance(linkDistance)
-			.linkStrength(linkStrength)
+      .on("tick", tick)
+      .linkDistance(linkDistance)
+      .linkStrength(linkStrength)
       .friction(0.5)
-      .gravity(0.7)
-			.charge(charge)
-			.size([ w, h ]);
+      .gravity(0.9)
+      .charge(charge)
+      .size([ w, h ]);
 
 // Add the svg object to the chart
 var vis = d3.select("#chart").append("svg:svg")
-	.attr("width", w)
-	.attr("height",	h);
+  .attr("width", w)
+  .attr("height", h);
 
 // Start polling for updates
 poll();
 
-//pollLocal(1);  // To debug with local data files in Firefox
+//pollLocal(4);  // To debug with local data files in Firefox
