8b893d53625debc87e65532e884780fde36e037b
William DeCoste <bdecoste@broker.example.com>
2015-01-16 08:57:46 -0800

    update

--- scripts/haproxy.rb
+++ scripts/haproxy.rb
@@ -8,6 +8,6 @@ gear = `rhc app-show --gears scale | tail -n +3 | awk '{print $6}'`.split[0]
 pid = `ssh #{gear} "ps aux" | grep "haproxy" | awk '{print $2}'`
 
 # Print out the listen part of the haproxy config
-config = `ssh #{gear} "cat haproxy-1.4/conf/haproxy.cfg" | tail -n +62`
+config = `ssh #{gear} "cat haproxy/conf/haproxy.cfg" | tail -n +62`
 puts "Process ID - #{pid}\n"
 puts "Config:\n#{config}"
